f3ae2da34cd0aaff5d89301f0f44c3b9fd194497
Daniel <archibasov.21.daniil.w@gmail.com>
2025-12-07 17:36:13 +0300

    add block with selection of target language

--- app/main.py
+++ app/main.py
@@ -3,39 +3,45 @@ from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import onnxruntime as ort
-import numpy as np
 from tokenization_small100 import SMALL100Tokenizer
 from onnx_greedy import greedy_generate_onnx
+from pydantic import BaseModel
 
 app = FastAPI()
 
-# Шаблоны (templates/index.html)
 templates = Jinja2Templates(directory="templates")
 
+class TranslationRequest(BaseModel):
+    text: str
+    flag: str | None = None
 
-# Загружаем ONNX модель (файл должен лежать рядом: app/model.onnx)
+# Загружаем модель
 tokenizer = SMALL100Tokenizer.from_pretrained("alirezamsh/small100")
 tokenizer.tgt_lang = "ru"
-#################################
+
 encoder_session = ort.InferenceSession("encoder_model_int8.onnx")
 decoder_session = ort.InferenceSession("decoder_model_int8.onnx")
-
-
-
 
 
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+
 @app.post("/predict")
-async def predict(request: Request):
-    payload = await request.json()
-    text = payload.get("text", "")
+async def predict(req: TranslationRequest):
+
+    text = req.text
+    flag = req.flag
+
     if not text:
         return JSONResponse({"error": "no text provided"}, status_code=400)
 
-    # Подготовка входов через токенизатор (возвращаем numpy массивы)
+    # Устанавливаем язык
+    if flag:
+        tokenizer.tgt_lang = flag
+
+    # Генерация
     output_ids = greedy_generate_onnx(
         encoder_session,
         decoder_session,
@@ -43,8 +49,9 @@ async def predict(request: Request):
         text,
         max_new_tokens=50
     )
+
     output_text = tokenizer.decode(output_ids[0], skip_special_tokens=True)
-    
-    # Преобразуем выход (обычно logits/ids) в удобный формат.
-    # Здесь просто вернём raw-первый выход как список.
-    return {"result": output_text}
+
+    return {"translation": output_text}   # <--- ВАЖНО!
+
+
